06dbdedb9e1e517184180f55ca7de401f7089aa1
Roilan <roilan14@gmail.com>
2016-04-18 10:52:53 -0400

    add option to render custom mailchimp attributes for designer view

--- src/renderEmail.js
+++ src/renderEmail.js
@@ -1,6 +1,12 @@
 import ReactDOMServer from 'react-dom/server'
 
-export default function renderEmail(emailComponent) {
+export default function renderEmail(emailComponent, { renderMailchimpAttr } = {}) {
   const doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
-  return doctype + ReactDOMServer.renderToStaticMarkup(emailComponent)
+  let markup = doctype + ReactDOMServer.renderToStaticMarkup(emailComponent)
+
+  if (renderMailchimpAttr) {
+    markup = markup.replace(/data-mc-/g, 'mc:')
+  }
+
+  return markup
 }
