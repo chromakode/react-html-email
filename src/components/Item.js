263c7271fd2cde7a9d967c7fa7228a152e5db108
martinbethann <martin.bethann@gmail.com>
2016-10-17 22:07:42 -0400

    Added ClassName prop to Item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ import includeDataProps from '../includeDataProps'
 export default function Item(props) {
   return (
     <tr>
-      <td {...includeDataProps(props)} align={props.align} valign={props.valign} bgcolor={props.bgcolor} style={props.style}>
+      <td {...includeDataProps(props)} className={props.className} align={props.align} valign={props.valign} bgcolor={props.bgcolor} style={props.style}>
         {props.children}
       </td>
     </tr>
@@ -13,6 +13,7 @@ export default function Item(props) {
 }
 
 Item.propTypes = {
+  className: PropTypes.string,
   bgcolor: PropTypes.string,
   align: PropTypes.oneOf(['left', 'center', 'right']),
   valign: PropTypes.oneOf(['top', 'middle', 'bottom']),
