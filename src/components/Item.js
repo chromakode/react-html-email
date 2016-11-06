6c097521ae028d5d9cc882870ae939dee8e323cd
Gabriel Prat Masramon <gabriprat@gmail.com>
2016-11-06 18:26:00 +0100

    Add dir attribute to Item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ import includeDataProps from '../includeDataProps'
 export default function Item(props) {
   return (
     <tr>
-      <td {...includeDataProps(props)} className={props.className} align={props.align} valign={props.valign} bgcolor={props.bgcolor} style={props.style}>
+      <td {...includeDataProps(props)} className={props.className} align={props.align} valign={props.valign} bgcolor={props.bgcolor} style={props.style} dir={props.dir}>
         {props.children}
       </td>
     </tr>
@@ -18,5 +18,6 @@ Item.propTypes = {
   align: PropTypes.oneOf(['left', 'center', 'right']),
   valign: PropTypes.oneOf(['top', 'middle', 'bottom']),
   style: EmailPropTypes.style,
+  dir: PropTypes.oneOf(['ltr','rtl']),
   children: PropTypes.node,
 }
