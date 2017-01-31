aa6c9c693dea51d9b591433cb7751f2b122a18c0
Wagner Moschini <wagnermoschini@gmail.com>
2017-01-31 00:05:18 -0200

    fix lint

--- src/components/Col.js
+++ src/components/Col.js
@@ -3,33 +3,33 @@ import EmailPropTypes from '../PropTypes'
 import includeDataProps from '../includeDataProps'
 
 export default function Col(props) {
-    return (
-        <td {...includeDataProps(props)}
-            className={props.className}
-            align={props.align}
-            valign={props.valign}
-            bgcolor={props.bgcolor}
-            colSpan={props.colSpan}
-            style={props.style}>
-            {props.children}
-        </td>
-    )
+  return (
+    <td {...includeDataProps(props)}
+      className={props.className}
+      align={props.align}
+      valign={props.valign}
+      bgcolor={props.bgcolor}
+      colSpan={props.colSpan}
+      style={props.style}>
+      {props.children}
+    </td>
+  )
 }
 
 Col.propTypes = {
-    className: PropTypes.string,
-    bgcolor: PropTypes.string,
-    colSpan: PropTypes.number,
-    align: PropTypes.oneOf(['left', 'center', 'right']),
-    valign: PropTypes.oneOf(['top', 'middle', 'bottom']),
-    style: EmailPropTypes.style,
-    children: PropTypes.node
-};
+  className: PropTypes.string,
+  bgcolor: PropTypes.string,
+  colSpan: PropTypes.number,
+  align: PropTypes.oneOf(['left', 'center', 'right']),
+  valign: PropTypes.oneOf(['top', 'middle', 'bottom']),
+  style: EmailPropTypes.style,
+  children: PropTypes.node,
+}
 
 Col.defaultProps = {
-    className: null,
-    bgcolor: null,
-    align: 'center',
-    valign: 'top',
-    style: null
-};
+  className: null,
+  bgcolor: null,
+  align: 'center',
+  valign: 'top',
+  style: null,
+}
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,31 +3,31 @@ import EmailPropTypes from '../PropTypes'
 import includeDataProps from '../includeDataProps'
 
 export default function Row(props) {
-    return (
-        <tr {...includeDataProps(props)}
-            className={props.className}
-            align={props.align}
-            valign={props.valign}
-            bgcolor={props.bgcolor}
-            style={props.style}>
-            {props.children}
-        </tr>
-    )
+  return (
+    <tr {...includeDataProps(props)}
+      className={props.className}
+      align={props.align}
+      valign={props.valign}
+      bgcolor={props.bgcolor}
+      style={props.style}>
+      {props.children}
+    </tr>
+  )
 }
 
 Row.propTypes = {
-    className: PropTypes.string,
-    bgcolor: PropTypes.string,
-    align: PropTypes.oneOf(['left', 'center', 'right']),
-    valign: PropTypes.oneOf(['top', 'middle', 'bottom']),
-    style: EmailPropTypes.style,
-    children: PropTypes.node,
+  className: PropTypes.string,
+  bgcolor: PropTypes.string,
+  align: PropTypes.oneOf(['left', 'center', 'right']),
+  valign: PropTypes.oneOf(['top', 'middle', 'bottom']),
+  style: EmailPropTypes.style,
+  children: PropTypes.node,
 }
 
 Row.defaultProps = {
-    className: null,
-    bgcolor: null,
-    align: 'center',
-    valign: 'top',
-    style: null
-};
+  className: null,
+  bgcolor: null,
+  align: 'center',
+  valign: 'top',
+  style: null,
+}
