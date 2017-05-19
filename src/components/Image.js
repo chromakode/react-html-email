ec0f949a1fa89a0090fb396e98521f43a339fcdf
Taylor Winfield <taylorwinfield4@gmail.com>
2017-05-19 21:29:48 +0100

    fix linting errors

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import EmailPropTypes from '../PropTypes'
 import includeDataProps from '../includeDataProps'
