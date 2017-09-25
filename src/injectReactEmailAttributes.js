8f32e5fa618f6f8f3a5e10f16e3ae0caec9ee7de
Max Goodman <c@chromakode.com>
2017-09-24 19:52:43 -0700

    Remove obsolete React 15.3.0 workaround

--- src/injectReactEmailAttributes.js
+++ src/injectReactEmailAttributes.js
@@ -6,7 +6,6 @@ export { DOMProperty as _DOMProperty }
 
 export const emailAttributes = {
   Properties: {
-    'xmlns': 0,
     'align': 0,
     'valign': 0,
     'bgcolor': 0,
@@ -19,11 +18,6 @@ let injected = false
 export default function injectReactEmailAttributes() {
   if (injected) {
     return
-  }
-
-  if (DOMProperty.properties.hasOwnProperty('xmlns')) {
-    // already exists in React 15.3.0
-    delete emailAttributes.Properties.xmlns
   }
 
   // make React accept some HTML attributes useful to emails
